2bada2ba764685810804195fa8cbdd09ac05c9f1
Dorivan Fernandes <dorivanfernandes@outlook.com>
2022-02-27 13:22:57 -0300

    criando relatório

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -2,6 +2,7 @@ package br.com.alura.forum.service
 
 import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
+import br.com.alura.forum.dto.TopicoPorCategoriaDto
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.exceptions.NotFoundException
 import br.com.alura.forum.mapper.TopicoFormMapper
@@ -54,6 +55,10 @@ class TopicoService(
     fun deletar(id: Long) {
         topicoRepository.deleteById(id)
 
+    }
+
+    fun relatorio(): List<TopicoPorCategoriaDto> {
+        return topicoRepository.relatorio()
     }
 
 
